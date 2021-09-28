baac0ac9e33fc7cfbb8f68b769b3c159b56255ac
Eleanor Roberts <eleanorlroberts@gmail.com>
2021-09-28 10:22:06 +0100

    removed whitespace and extra lines

--- collectionchallenge.php
+++ collectionchallenge.php
@@ -7,11 +7,8 @@ require_once "collectiondb.php";
 <head>
     <meta charset="UTF-8" />
     <title>Pokemon Collection</title>
-
     <link rel="stylesheet" href="normalize.css" />
-    <!--	<link rel="stylesheet" href="xxx.css" />-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
 </head>
 <body>
 <h1>Pokemonz</h1>
