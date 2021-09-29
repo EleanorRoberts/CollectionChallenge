98a9c4c134ef7e7e80327a8e0d604aa1f8ca8b8d
Eleanor Roberts <eleanorlroberts@gmail.com>
2021-09-29 10:22:20 +0100

    attempted to fix addPoke function

--- addpokemon.php
+++ addpokemon.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'collectionfunctions.php';
 $db = connectToDB('pokemon');
-checkNew($db);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,13 +16,15 @@ checkNew($db);
     <label for="name">Name <input type="text" name="name" required /></label>
     <label for="type1">First Type <input type="text" name="type1" required/></label>
     <label for="type2">Second Type <input type="text" name="type2"/></label>
-    <label for="hp">HP <input type="text" name="hp" required/></label>
-    <label for="attack">Attack <input type="text" name="attack" required /></label>
-    <label for="defense">Defense <input type="text" name="defense" required /></label>
-    <label for="spAttack">Sp. Attack <input type="text" name="spAttack" required /></label>
-    <label for="spDefense">Sp. Defense <input type="text" name="spDefense" required/></label>
-    <label for="speed">Speed <input type="text" name="speed" required/></label>
+    <label for="hp">HP <input type="number" name="hp" required/></label>
+    <label for="attack">Attack <input type="number" name="attack" required /></label>
+    <label for="defense">Defense <input type="number" name="defense" required /></label>
+    <label for="spAttack">Sp. Attack <input type="number" name="spAttack" required /></label>
+    <label for="spDefense">Sp. Defense <input type="number" name="spDefense" required/></label>
+    <label for="speed">Speed <input type="number" name="speed" required/></label>
     <input type="submit" name="submit" />
+    <?php checkNew($db);
+    ?>
 </form>
 </body>
 </html>
