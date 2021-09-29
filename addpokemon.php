e5a76309cbe5d3a3f62bc1259576fb724262d3de
Eleanor Roberts <eleanorlroberts@gmail.com>
2021-09-29 14:06:37 +0100

    added functionality to the edit pokemon button

--- addpokemon.php
+++ addpokemon.php
@@ -1,13 +1,14 @@
 <?php
 require_once 'collectionfunctions.php';
 $db = connectToDB('pokemon');
+$pokemon = collectData($db);
 checkNew($db);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
-    <title>Pokemon Collection</title>
+    <title>Add a Pokemon</title>
     <?php echo addStyle(); ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
