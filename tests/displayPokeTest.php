fd0d754b57ccb8230e61b0eb97f2cd3505a83971
Eleanor Roberts <eleanorlroberts@gmail.com>
2021-09-30 16:23:33 +0100

    changed tests to function

--- tests/displayPokeTest.php
+++ tests/displayPokeTest.php
@@ -7,9 +7,10 @@ class displayPokeTest extends TestCase
 {
     public function testSuccessDisplayPokeOneType()
     {
-        $expected = "<div><h2>Nova</h2><p>Cutie</p><ul><li>HP: 1</li><li>Attack: 2</li><li>Defense: 3</li><li>Sp. Attack: 4</li><li>Sp. Defense: 5</li><li>Speed: 6</li></div>";
+        $expected = "<div class='pokeCard'><h2>Nova</h2><p class='type'>Cutie</p><table><tr><th>Stat</th><th>Value</th></tr><tr><td>HP: </td><td>1</td></tr><tr><td>Attack: </td><td>2</td></tr><tr><td>Defense: </td><td>3</td></tr><tr><td>Sp. Attack: </td><td>4</td></tr><tr><td>Sp. Defense: </td><td>5</td></tr><tr><td>Speed: </td><td>6</td></tr></table><a href='editpokemon.php?id=12' class='edit'>Edit</a><a href='deletePokemon.php?id=12' class='delete'>Delete</a></div>";
 
         $input[0] = [
+            "id" => "12",
             "name" => "Nova",
             "type1" => "Cutie",
             "type2" => "",
@@ -28,9 +29,10 @@ class displayPokeTest extends TestCase
 
     public function testSuccessDisplayPokeTwoTypes()
     {
-        $expected = "<div><h2>Nova</h2><p>Cutie & Pie</p><ul><li>HP: 1</li><li>Attack: 2</li><li>Defense: 3</li><li>Sp. Attack: 4</li><li>Sp. Defense: 5</li><li>Speed: 6</li></div>";
+        $expected = "<div class='pokeCard'><h2>Nova</h2><p class='type'>Cutie & Pie</p><table><tr><th>Stat</th><th>Value</th></tr><tr><td>HP: </td><td>1</td></tr><tr><td>Attack: </td><td>2</td></tr><tr><td>Defense: </td><td>3</td></tr><tr><td>Sp. Attack: </td><td>4</td></tr><tr><td>Sp. Defense: </td><td>5</td></tr><tr><td>Speed: </td><td>6</td></tr></table><a href='editpokemon.php?id=12' class='edit'>Edit</a><a href='deletePokemon.php?id=12' class='delete'>Delete</a></div>";
 
         $input[0] = [
+            "id" => "12",
             "name" => "Nova",
             "type1" => "Cutie",
             "type2" => "Pie",
