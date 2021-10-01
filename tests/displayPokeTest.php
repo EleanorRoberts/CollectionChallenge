d2bb41326badae565bbbc193f90b1fe23140cb1f
Eleanor Roberts <eleanorlroberts@gmail.com>
2021-10-01 11:23:56 +0100

    fixed unit tests

--- tests/displayPokeTest.php
+++ tests/displayPokeTest.php
@@ -7,9 +7,10 @@ class displayPokeTest extends TestCase
 {
     public function testSuccessDisplayPokeOneType()
     {
-        $expected = "<div><h2>Nova</h2><p>Cutie</p><ul><li>HP: 1</li><li>Attack: 2</li><li>Defense: 3</li><li>Sp. Attack: 4</li><li>Sp. Defense: 5</li><li>Speed: 6</li></div>";
+        $expected = "<div class='pokeCard'><h2>Nova</h2><p class='type Cutie'>Cutie</p><ul><li>HP: 1</li><li>Attack: 2</li><li>Defense: 3</li><li>Sp. Attack: 4</li><li>Sp. Defense: 5</li><li class='speed'>Speed: 6</li><a href='editpokemon.php?id=12' class='edit'>Edit</a><a href='deletePokemon.php?id=12' class='delete'>Delete</a></div>";
 
         $input[0] = [
+            "id" => 12,
             "name" => "Nova",
             "type1" => "Cutie",
             "type2" => "",
@@ -28,9 +29,10 @@ class displayPokeTest extends TestCase
 
     public function testSuccessDisplayPokeTwoTypes()
     {
-        $expected = "<div><h2>Nova</h2><p>Cutie & Pie</p><ul><li>HP: 1</li><li>Attack: 2</li><li>Defense: 3</li><li>Sp. Attack: 4</li><li>Sp. Defense: 5</li><li>Speed: 6</li></div>";
+        $expected = "<div class='pokeCard'><h2>Nova</h2><p class='type CutiePie'>Cutie & Pie</p><ul><li>HP: 1</li><li>Attack: 2</li><li>Defense: 3</li><li>Sp. Attack: 4</li><li>Sp. Defense: 5</li><li class='speed'>Speed: 6</li><a href='editpokemon.php?id=12' class='edit'>Edit</a><a href='deletePokemon.php?id=12' class='delete'>Delete</a></div>";
 
         $input[0] = [
+            "id" => 12,
             "name" => "Nova",
             "type1" => "Cutie",
             "type2" => "Pie",
